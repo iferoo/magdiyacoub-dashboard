63b8f2726643d4d78721eaa1b519cdeba2a07c4f
Eyad Mohamed <eyadmohamed2859@gmail.com>
2022-08-11 20:45:23 +0200

    handle update staff styled

--- src/components/staff/Staff.Update.js
+++ src/components/staff/Staff.Update.js
@@ -3,8 +3,8 @@ import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useOutletContext } from 'react-router-dom';
 
-import styled from 'styled-components';
 import { updateDoctor } from '../../store/doctorSlice';
+import { UpdateStaffSection } from '../../styles/Staff.Styled';
 
 export default function UpdateStaff() {
   const dispatch = useDispatch();
@@ -47,7 +47,7 @@ export default function UpdateStaff() {
   // console.log(errors);
 
   return (
-    <Section>
+    <UpdateStaffSection>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="profile">
           <div className="inputAlign">
@@ -168,82 +168,7 @@ export default function UpdateStaff() {
           <input type="submit" value="Update" />
         </div>
       </form>
-    </Section>
+    </Updat>
   );
 }
 
-const Section = styled.section`
-  width: 100%;
-  box-shadow: 0px 0px 1px 1px #888888;
-  .line {
-    width: 80%;
-    margin: 0 auto;
-    border: 0.3px solid #b9b9b9;
-    border-radius: 1rem;
-  }
-
-  input,
-  select {
-    padding: 0.5rem;
-
-    &::placeholder {
-    }
-    &:focus {
-    }
-  }
-  .profile {
-    padding: 1rem 3rem;
-    display: flex;
-    flex-direction: column;
-    gap: 0.3rem;
-    .inputAlign {
-      display: flex;
-      align-items: center;
-      label {
-        width: 40%;
-      }
-      input {
-        width: 60%;
-      }
-      select {
-        width: 60%;
-      }
-    }
-  }
-
-  .submit {
-    padding: 1rem 3rem;
-    input {
-      width: 100%;
-      margin: 0.5rem auto;
-      cursor: pointer;
-      color: #fff;
-      text-align: center;
-      background-color: #0d6efd;
-      border-color: #0d6efd;
-      font-size: 1rem;
-      transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-        border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-      border: none;
-      border-radius: 0.2rem;
-      padding: 0.5rem;
-      &:hover {
-        color: #fff;
-        background-color: #0b5ed7;
-        border-color: #0a58ca;
-      }
-    }
-  }
-
-  @media screen and (min-width: 720px) and (max-width: 1080px) {
-  }
-
-  @media screen and (min-width: 280px) and (max-width: 720px) {
-    .profile {
-      padding: 1rem;
-    }
-    .submit {
-      padding: 1rem;
-    }
-  }
-`;
--- src/styles/Staff.Styled.js
+++ src/styles/Staff.Styled.js
@@ -104,3 +104,79 @@ export const AddStaffSection = styled.section`
     }
   }
 `;
+
+export const UpdateStaffSection = styled.section`
+  width: 100%;
+  box-shadow: 0px 0px 1px 1px #888888;
+  .line {
+    width: 80%;
+    margin: 0 auto;
+    border: 0.3px solid #b9b9b9;
+    border-radius: 1rem;
+  }
+
+  input,
+  select {
+    padding: 0.5rem;
+
+    &::placeholder {
+    }
+    &:focus {
+    }
+  }
+  .profile {
+    padding: 1rem 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+    .inputAlign {
+      display: flex;
+      align-items: center;
+      label {
+        width: 40%;
+      }
+      input {
+        width: 60%;
+      }
+      select {
+        width: 60%;
+      }
+    }
+  }
+
+  .submit {
+    padding: 1rem 3rem;
+    input {
+      width: 100%;
+      margin: 0.5rem auto;
+      cursor: pointer;
+      color: #fff;
+      text-align: center;
+      background-color: #0d6efd;
+      border-color: #0d6efd;
+      font-size: 1rem;
+      transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+        border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+      border: none;
+      border-radius: 0.2rem;
+      padding: 0.5rem;
+      &:hover {
+        color: #fff;
+        background-color: #0b5ed7;
+        border-color: #0a58ca;
+      }
+    }
+  }
+
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 720px) {
+    .profile {
+      padding: 1rem;
+    }
+    .submit {
+      padding: 1rem;
+    }
+  }
+`;
